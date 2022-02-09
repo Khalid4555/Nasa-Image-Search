c6deaae6e480a43f3af865aed611cd578ace2541
Khalid Nur <kingnur36@gmail.com>
2022-02-09 20:13:54 +0000

    setup project and added a search component

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/App.css";
+import Search from "./Search";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="nasaLogo"
+        width="300px"
+      />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
